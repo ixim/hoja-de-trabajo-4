1518375d64d4a5da235fc7c2aa345d5dd4b598d4
Dila Margarita <Dila Margarita@Dila>
2014-08-02 19:37:51 -0600

    primer proyecto

--- src/calculadora/StackCalculadora.java
+++ src/calculadora/StackCalculadora.java
@@ -42,15 +42,15 @@ public class StackCalculadora
 				
 				if (caracter1==49||caracter1==50||caracter1==51||caracter1==52||caracter1==53||caracter1==54||caracter1==55||caracter1==56||caracter1==57)//si caracter es un numero
 				{
-					lista.push(String.valueOf((char)caracter1));//convierte caracter a char para luego pasarlo a String, se guarda en forma de string en el stack
+					miStack.push(String.valueOf((char)caracter1));//convierte caracter a char para luego pasarlo a String, se guarda en forma de string en el stack
 				}
 				if (caracter1 == 43)//suma
 				{
-					caracter1 = Integer.parseInt(lista.pop());//retorna primer valor ingresado en el stack
-					caracter2 = Integer.parseInt(lista.pop());//retorn segundo valor ingresado en el stack
+					caracter1 = Integer.parseInt(miStack.pop());//retorna primer valor ingresado en el stack
+					caracter2 = Integer.parseInt(miStack.pop());//retorn segundo valor ingresado en el stack
 					resultado = caracter1 + caracter2;//suma valores
 					System.out.print(caracter2+"+"+caracter1+"="+resultado+"\n");//imprime el resultado de la operacion realizada
-					lista.push(String.valueOf(resultado));//ingresa el resultado en el stack
+					miStack.push(String.valueOf(resultado));//ingresa el resultado en el stack
 				}
 				if (caracter1 == 45)//resta
 				{
@@ -58,7 +58,7 @@ public class StackCalculadora
 					caracter2 = Integer.parseInt(lista.pop());
 					resultado = caracter2-caracter1;//resta valores
 					System.out.print(caracter2+"-"+caracter1+"="+resultado+"\n");
-					lista.push(String.valueOf(resultado));//ingresa el resultado en el stack
+					miStack.push(String.valueOf(resultado));//ingresa el resultado en el stack
 				}
 				if (caracter1 == 42)//multiplicacion
 				{
@@ -66,7 +66,7 @@ public class StackCalculadora
 					caracter2 = Integer.parseInt(lista.pop());
 					resultado = caracter1*caracter2;//multiplica valores
 					System.out.print(caracter2+"*"+caracter1+"="+resultado+"\n");
-					lista.push(String.valueOf(resultado));//ingresa el resultado en el stack
+					miStack.push(String.valueOf(resultado));//ingresa el resultado en el stack
 				}
 				if (caracter1 == 47)//division
 				{
@@ -74,7 +74,7 @@ public class StackCalculadora
 					caracter2 = Integer.parseInt(lista.pop());
 					resultado = caracter2/caracter1;//divide valores
 					System.out.print(caracter2+"/"+caracter1+"="+resultado+"\n");
-					lista.push(String.valueOf(resultado));//ingresa el resultado en el stack
+					miStack.push(String.valueOf(resultado));//ingresa el resultado en el stack
 				}
 			}
 			while(caracter1 != -1); //Se recorre el fichero hasta encontrar el carácter -1 que marca el final del fichero
