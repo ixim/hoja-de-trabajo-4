d188c5760d27208bed09d994232f14a8e654aeda
Dila Margarita <Dila Margarita@172.17.5.169>
2014-08-04 18:43:51 -0600

    modificando

--- src/calculadora/StackFactory.java
+++ src/calculadora/StackFactory.java
@@ -10,8 +10,9 @@ class StackFactory<E>
     // seleccion de la implementacion a utilizar:
 	if (entry.equals("AL"))
 		return new StackArrayList<E>(); //regresa ArrayList
-	/*if (entry.equals("V"))
-		return new StackVector<E>(); //regresa Vector*/
+	/*if (entry.equals("L"))
+                StackSingleton<String> sSingleton = new StackSingleton<String>();
+                StackLista<String> lista = sSingleton.getStackLista(a);*/
 	else
 		return new StackVector<E>(); //regresa Vector
 	}
